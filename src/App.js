59eeeedac12f933bc0ad192a433cb7ec75d862a1
Sonamrit <lama77916@gmail.com>
2025-04-23 22:33:49 +0545

    Updated App.js, Navbar.js, and TextForm.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
+import TextForm from './components/TextForm';
 import About from './components/About';
 import Navbar from './components/Navbar';
+import { useState } from 'react';
 // import TextForm from './components/TextForm';
 
 
 function App() {
+  const [mode, setMode] = useState('light'); //Whether dark mode is enabled or not 
+
+  const toggleMode =() =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+  }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor = 'white';
+  }
+}
+
+
   return (
     <>
-<Navbar title="TextUtil" aboutText="About"/>
+<Navbar title="TextUtil" mode={mode} toggleMode={toggleMode}/>
 <div className="container my-3">
-{/* <TextForm heading="Enter the text to analyze below"/> */}
+<TextForm heading="Enter the text to analyze below" mode={mode}/>
 <About/>
 </div>
     </>
